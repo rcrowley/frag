b1358618acd13a4a8f2c6e4450420886be9cfee0
Richard Crowley <r@rcrowley.org>
2025-01-04 22:11:01 -0800

    Nest stuff under testdata/

--- main_test.go
+++ main_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestMainH1(t *testing.T) {
 	stdout := &bytes.Buffer{}
-	Main([]string{"frag", "<h1>", "test.html"}, os.Stdin, stdout)
+	Main([]string{"frag", "<h1>", "testdata/test.html"}, os.Stdin, stdout)
 	if s := stdout.String(); s != "<h1>Hello, world!</h1>\n" {
 		t.Fatal(s)
 	}
@@ -16,7 +16,7 @@ func TestMainH1(t *testing.T) {
 
 func TestMainTitle(t *testing.T) {
 	stdout := &bytes.Buffer{}
-	Main([]string{"frag", "<title>", "test.html"}, os.Stdin, stdout)
+	Main([]string{"frag", "<title>", "testdata/test.html"}, os.Stdin, stdout)
 	if s := stdout.String(); s != "<title>Hello, world!</title>\n" {
 		t.Fatal(s)
 	}
