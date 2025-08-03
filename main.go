bb057f5ab81562d9388bda87c936543b055adb20
Richard Crowley <rcrowley@planetscale.com>
2025-08-02 20:36:45 -0700

    lol must and must2 actually need to exit, come on

--- main.go
+++ main.go
@@ -82,12 +82,16 @@ func main() {
 
 func must(err error) {
 	if err != nil {
-		log.Fatal(err)
+		log.Output(2, err.Error())
+		os.Exit(1)
 	}
 }
 
 func must2[T any](v T, err error) T {
-	must(err)
+	if err != nil {
+		log.Output(2, err.Error())
+		os.Exit(1)
+	}
 	return v
 }
 
